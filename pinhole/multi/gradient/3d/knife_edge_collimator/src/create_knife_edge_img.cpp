b5fe96979d5af889672461f15de43fa898cb6e6c
eeyore114 <pengin.01.30.baseball7@gmail.com>
2019-07-28 15:44:23 +0900

    create finction 'calculate cross point'

--- pinhole/multi/gradient/3d/knife_edge_collimator/src/create_knife_edge_img.cpp
+++ pinhole/multi/gradient/3d/knife_edge_collimator/src/create_knife_edge_img.cpp
@@ -41,6 +41,22 @@ typedef struct {
 	float knife_edge_theta_degree;
 } Condition;
 
+
+typedef struct {
+	// y = (grad)x + intercept とする
+	float grad;
+	float intercept;
+} Equation1d;
+
+
+void create_collimator_img(std::vector<float> &detector, Condition cond, std::vector<float> pinhole_center, std::vector<float> pinhole_theta_xy, std::vector<float> pinhole_theta_yz);
+void set_cross_point(std::vector<Eigen::Vector3f> &cross_point, Condition cond, std::vector<float> pinhole_center, std::vector<float> pinhole_theta_xy, std::vector<float> pinhole_theta_yz);
+Equation1d calc_liner_equation(Eigen::Vector3f start, Eigen::Vector3f end);
+Eigen::Vector3f calc_cross_point(Eigen::Vector3f left_start, Eigen::Vector3f left_end, Eigen::Vector3f right_start, Eigen::Vector3f right_end);
+
+
+
+
 int main()
 {
 	Condition cond;
@@ -76,8 +92,13 @@ int main()
 
 	/*---- 時間計測開始&条件表示  end -----*/
 
+
+
+
+
 	// 変数の定義
 	std::vector<float> detector(cond.detector_size_w * cond.detector_size_h, 0.);
+	create_collimator_img(detector, cond, pinhole_center, pinhole_theta_xy, pinhole_theta_yz);
 
 	// 関数呼び出し
 	// launch_test_gradient_pinhole(detector, pinhole_theta_xy, pinhole_theta_yz, pinhole_center, cond);
@@ -87,7 +108,7 @@ int main()
 	/*----- 時間計測処理 end-------*/
 }
 
-void create_collimator_img()
+void create_collimator_img(std::vector<float> &detector, Condition cond, std::vector<float> pinhole_center, std::vector<float> pinhole_theta_xy, std::vector<float> pinhole_theta_yz)
 {
 	/*
 		- 2つのエッジの交点を求める
@@ -95,6 +116,98 @@ void create_collimator_img()
 			- 表面の座標を見て，距離が近い方と内積を比較　
 	*/
 
+	/* --- Eigen の配列を作れるらしい --- */
+	// std::vector<Eigen::Vector3f> v(2);
+	// v[0] << 1, 2, 3;
+	// v[1] << 4, 5, 6;
+	// // for(int i = 0; i < 2; i++) std::cout << v[i] << std::endl;
+	//
+	// std::cout << v[0](1) << '\n';
+	/* --- Eigen の配列を作れるらしい --- */
 
-	
+
+
+
+
+
+
+
+	std::vector<Eigen::Vector3f> cross_point_edge(pinhole_theta_xy.size());
+	set_cross_point(cross_point_edge, cond, pinhole_center, pinhole_theta_xy, pinhole_theta_yz);
+
+
+
+
+
+
+
+
+
+	/*---- ナイフエッジの交点を求める -----*/
+	// Eigen::Vector3f cross_point_edge;
+	// {
+	// 	float x1 = cond.collimator_w / 2.;
+	// 	float y1 = -cond.collimator_h / 2.;
+	// 	float x2 = cond.collimator_w / 2. + (cond.collimator_h / 2.) * tan(M_PI / 6.);
+	// 	float y2 = -cond.collimator_h;
+	// 	float t = - x1 / (x2 - x1);
+	// 	float y = y1 + t * (y2 - y1);
+	//
+	// 	cross_point_edge << 0, y, 0;
+	// }
+
+
+
+
+	/*
+	・決まった座標がないから今までみたいに求めることができない．
+	・tの求め方がわからない
+
+
+	*/
+
+
+
+
+
+}
+
+
+void set_cross_point(std::vector<Eigen::Vector3f> &cross_point, Condition cond, std::vector<float> pinhole_center, std::vector<float> pinhole_theta_xy, std::vector<float> pinhole_theta_yz)
+{
+	/*
+	pinholeの数だけループ
+	4点を求める
+	func : calc_cross_point
+
+	*/
+	for(int i = 0; i < pinhole_theta_xy.size(); i++)
+	{
+		Eigen::Vector3f left_start, left_end, right_start, right_end;
+		// 4点を求める
+
+		cross_point[i] = calc_cross_point(left_start, left_end, right_start, right_end);
+	}
+}
+
+
+Equation1d calc_liner_equation(Eigen::Vector3f start, Eigen::Vector3f end)
+{
+	Equation1d equation;
+	equation.grad = (end(1) - start(1)) / (end(0) - start(0));
+	equation.intercept = start(1) - equation.grad * start(0);
+	return equation;
+}
+
+Eigen::Vector3f calc_cross_point(Eigen::Vector3f left_start, Eigen::Vector3f left_end, Eigen::Vector3f right_start, Eigen::Vector3f right_end)
+{
+	Equation1d left, right;
+	left = calc_liner_equation(left_start, left_end);
+	right = calc_liner_equation(right_start, right_end);
+	Eigen::Vector3f cross_point;
+	// zはそのまま，xy平面で2直線の交点を求める
+	cross_point(2) = left_start(2);
+	cross_point(0) = (right.intercept - left.intercept) / (right.grad - left.grad);
+	cross_point(1) = right.grad * cross_point(0) + right.intercept;
+	return cross_point;
 }
